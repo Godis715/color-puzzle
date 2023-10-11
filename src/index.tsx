fb3c971a305da6ba5be12f1363c4ad5f8b50c1ad
Denis Ignatev <godis7.15@gmail.com>
2023-10-11 10:40:09 +0300

    Added material ui

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,11 @@ import { store } from 'store';
 
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(
