82bc18774a67a9176ffa71b881bc3b0f16c0b75b
Denis Ignatev <godis7.15@gmail.com>
2023-10-20 20:39:42 +0300

    Improved complexity estimator

--- src/shared/lib/graph-complexity-estimator.ts
+++ src/shared/lib/graph-complexity-estimator.ts
@@ -33,6 +33,27 @@ function shuffleGraph(graph: Graph, random: () => number): Graph {
   return renameNodes(graph, nodesMapping);
 }
 
+function removeNode(graph: Graph, n: number): Graph {
+  return graph
+    .filter((_, i) => i !== n)
+    .map(
+      (neighbors) => neighbors
+        .filter((i) => i !== n)
+        .map((i) => i < n ? i : i - 1)
+    )
+}
+
+function removeDangling(graph: Graph): Graph {
+  const dangling = graph
+    .map((_, i) => i)
+    .filter((i) => graph[i].length < 2);
+  
+  return dangling.reduce(
+    (acc, n) => removeNode(acc, n),
+    graph
+  );
+}
+
 export function estimateComplexity(
   graph: Graph,
   optimalSolution: number,
@@ -41,9 +62,10 @@ export function estimateComplexity(
 ): number {
   let correctResultsCount = 0;
   const random = seedrandom(seed.toString());
+  const cleared = removeDangling(graph);
 
   for (let i = 0; i < iter; i += 1) {
-    const shuffledGraph = shuffleGraph(graph, random);
+    const shuffledGraph = shuffleGraph(cleared, random);
     const coloring = getGraphColoringGreedy(shuffledGraph);
     const greedySolution = Math.max(...coloring) + 1;
 
@@ -53,4 +75,36 @@ export function estimateComplexity(
   }
 
   return 1 - correctResultsCount / iter;
+}
+
+export function generateGraph(n: number, m: number): Graph {
+  if (m > (n * (n - 1)) / 2) throw Error('m is too big');
+
+  const graph = new Array(n).fill(null).map(() => [] as number[]);
+  const generated = new Set();
+
+  let i = 0;
+
+  while (i < m) {
+    const randIdx = Math.floor(n * n * Math.random());
+
+    let n1 = randIdx % n;
+    let n2 = Math.floor(randIdx / n);
+
+    if (n1 === n2) continue;
+
+    if (n1 > n2) [n1, n2] = [n2, n1];
+
+    const key = `${n1},${n2}`;
+
+    if (generated.has(key)) continue;
+
+    generated.add(key);
+    graph[n1].push(n2);
+    graph[n2].push(n1);
+
+    i += 1;
+  }
+
+  return graph;
 }
